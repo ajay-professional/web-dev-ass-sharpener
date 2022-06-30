e82d62494ed12bd891219172d593ac6c70b09df2
Ajay <ajtheajay03@gmail.com>
2022-06-30 20:40:35 +0530

    promises code assignment

--- notepad web dev/promises.js
+++ notepad web dev/promises.js
@@ -1,38 +1,109 @@
-const posts=[{title:'Post One',body:'This is post one', createdAt:new Date().getTime()},
-{title:'Post Two', body: 'This is post two',createdAt:new Date().getTime()}];
-let intervalId=0;
+const posts=[{title:'Post One', body:'This is post one'},
+        {title:'Post Two', body: 'This is post two'},
+        {title:'Post three', body:'This is post three'}
+];
 function getPosts(){
-    clearInterval(intervalId);
-    intervalId=setInterval(()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
         let output='';
         posts.forEach((post)=>{
-        output+=`<li>${post.title} - created ${(new Date().getTime() - post.createdAt)/1000} seconds ago.</li>`;
-        })
+        output+=`<li>${post.title}.</li>`;
+        });
         document.body.innerHTML=output;
-        },1000);
+        const error=false;
+        if(!error){
+        resolve();
+        }
+        else{
+        reject('Error: Something went wrong');
+        }
+        },3000);
+    });
+    }       
+/*function createPost(post){
+    return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+    posts.push(post);
+    const error=false;
+    if(!error){
+        resolve(post);
     }
-function createPost(oost, callback){
-    setTimeout(()=>{
-    posts.push({...oost, createdAt:new Date().getTime()});
-    callback();
-    },2000);
+    else{
+        reject('Error: Something went wrong');
+    }
+    updateLastUserActivityTime();
+    }, 2000);
+});
+}*/
+function deletePost(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(posts.values!==0){
+                resolve();
+            }
+            else{
+                reject();
+            }
+        }, 1000);
+    });
+}
+/*createPost({title:'Post four', body:'This is post four'}).then(()=>{
+    getPosts()}).catch((err)=>console.log(err)).then(()=>{deletePost().then(()=>{posts.pop();}).catch(()=>console.log('Array is empty'))}).catch((err)=>console.log(err));
 
-}
-function create4thPost(post, callback){
-    setTimeout(()=>{
-        posts.push({...post, createdAt:new Date().getTime()});
-        callback();
-    },3000)
-}
-createPost({title:'Post three',body:'This is post three'},create4thPost({title:'Post fourth', body:'This is post fourth'}, getPosts));
+//deletePost().then(()=>{posts.pop();}).catch(()=>console.log('Array is empty'));
+//deletePost().then(()=>{posts.pop();}).catch(()=>console.log('Array is empty'));
 
-var timer;
-var count=0;
-function lastEditedInSecondsAgo(){
-    count=0;
-    clearInterval(timer)
-    timer=setInterval(()=>{
-        count++;
-        console.log(count)
-    },5000);
+/*getPosts();
+deletePost().then(()=>{
+    posts.pop();
+}).catch(()=>console.log('Array is empty'));
+deletePost().then(()=>{
+    posts.pop();
+}).catch(()=>console.log('Array is empty'));*/
+/*then(()=>{
+    deletePost().then(()=>{
+        posts.pop();
+    }).catch(()=>console.log('Array is empty'));
+})})*/
+/*let p1=new Promise(function(resolve,reject){
+    resolve("First");
+});
+let p2=new Promise(function(resolve,reject){
+    resolve("second");
+});
+Promise.all([p1,p2]).then(msg=>console.log(msg));*/
+const user={
+    username: 'Ajay',
+    lastActivityTime:'30th of June'
 }
+/*let updateLastUserActivityTime=function (){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            user.lastActivityTime=new Date().getTime();
+            resolve(user.lastActivityTime);
+        },1000);
+    });
+};*/
+//createPost({title:'Post four', body:'This is post four'});
+//updateLastUserActivityTime();*/
+Promise.all([new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+    posts.push({title:'Post four', body:'This is post four'});
+    const error=false;
+    if(!error){
+        resolve({title:'Post four', body:'This is post four'});
+    }
+    else{
+        reject('Error: Something went wrong');
+    }
+    //updateLastUserActivityTime();
+    }, 1000);
+}),new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        user.lastActivityTime=new Date().getTime();
+        resolve(user.lastActivityTime);
+    },1000);
+})]).then((data)=>{
+    console.log(data);
+}).catch(err=>console.log(err));
+
