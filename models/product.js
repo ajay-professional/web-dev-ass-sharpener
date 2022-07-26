421297d53bd1b3d6886f112b6592d6c6668248d4
Ajay <ajtheajay03@gmail.com>
2022-07-26 18:21:57 +0530

    Making SQL easy with SEQUELIZE

--- models/product.js
+++ models/product.js
@@ -1,32 +1,24 @@
-const db = require('../util/Database');
-
-const Cart = require('./cart');
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Sequelize = require('sequelize');
+const sequelize = require('../util/Database');
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
-
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?,?,?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById(id) {
-    return db.execute('DELETE FROM products WHERE products.id= ?', [id]);
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id= ?', [id]);
-  }
-};
+});
+module.exports = Product;
