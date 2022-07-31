baad455da626b962715a71156c5beef8e6503e6f
Ajay <ajtheajay03@gmail.com>
2022-07-31 14:40:39 +0530

    toast notification

--- ecommerceweb.js
+++ ecommerceweb.js
@@ -1,7 +1,20 @@
-const ham=document.getElementById('ham');
+const ham = document.getElementById('ham');
 const nav = document.getElementById('nav');
 
 ham.addEventListener("click", () => {
     nav.classList.toggle("active");
     ham.classList.toggle("active");
 });
+const cont = document.getElementById('container');
+document.getElementById('btn1').addEventListener('click', () => {
+    showNotification();
+});
+function showNotification() {
+    const dev = document.createElement('p');
+    dev.classList.add('toast');
+    dev.innerHTML = 'This challenge is crazy !';
+    cont.appendChild(dev);
+    setTimeout(() => {
+        dev.remove();
+    }, 3000);
+}
